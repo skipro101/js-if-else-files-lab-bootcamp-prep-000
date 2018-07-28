0803c2b5d1afc1cf0d3216e590e07c9a3f0a4426
Allen Freeman <freeman.allen@gmail.com>
2018-07-28 16:58:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 var name = "Joe"
 var height = "74"
-if (name === "Susan") {
-  console.log("The name is correct")
-} else {
-  console.log("Expected Susan")
-}
-
+ 
 // Don't worry about this
 module.exports = { name, height
 }
-
