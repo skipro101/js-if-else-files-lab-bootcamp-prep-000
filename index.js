d7b56ed73f9d08cd099ddca906ee1aaf06046f40
Allen Freeman <freeman.allen@gmail.com>
2018-07-28 16:57:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var name = "Joe"
 var height = "74"
 if (name === "Susan") {
   console.log("The name is correct")
-  else ("expected name to be Susan")
+} else {
+  console.log("Expected Susan")
 }
 
 // Don't worry about this
